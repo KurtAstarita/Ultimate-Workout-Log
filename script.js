5616ea2cce7a9ac8fd5fa3df6d47f778ce53728b
KurtAstarita <itskurtastarita@gmail.com>
2025-04-30 13:36:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,7 @@ function sendHeightToParent() {
         document.body.offsetHeight,
         document.documentElement.offsetHeight
     );
-    window.parent.postMessage({ height: contentHeight }, 'https://kurtastarita.github.io'); // Make sure this origin is correct
+    window.parent.postMessage({ height: contentHeight }, 'https://post40gains-fitness-tools.kurtastarita.com/ultimate-workout-log'); // Make sure this origin is correct
 }
 
 // Call sendHeightToParent whenever the content height changes
